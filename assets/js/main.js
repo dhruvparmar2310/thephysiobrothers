43536a02c05dccbe78cbe85e0570adfb8d6e0818
Dhruv Parmar <65495463+dhruvparmar2310@users.noreply.github.com>
2022-08-24 16:32:37 +0530

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -265,4 +265,45 @@
     })
   });
 
+  /**
+   * Blog Slider
+   */
+   new Swiper('.blogs-slider', {
+    speed: 2000,
+    loop: true,
+    centeredSlides: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    slidesPerView: 'auto',
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true
+    },
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+        spaceBetween: 21
+      },
+      640: {
+        slidesPerView: 3,
+        spaceBetween: 20
+      },
+      992: {
+        slidesPerView: 5,
+        spaceBetween: 20
+      }
+    }
+  });
+
+  /**
+   * Initiate gallery lightbox 
+   */
+  const blogsLightbox = GLightbox({
+    selector: '.gallery-lightbox'
+  });
+
+
 })()
